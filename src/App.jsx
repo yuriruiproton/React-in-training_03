4326098e8e42e1ab0c86c2ac2b530739690d721a
unknown <yurirui656@gmail.com.com>
2024-07-17 12:49:03 +0200

    Component Profile

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Profile from "./components/Profile"
+import image from "./assets/pic.jpg"
+
+function App() {
+ 
+ return(
+    <div className="app">
+        <Profile 
+              avatar={image} 
+              name="Harvey Specter"
+              bio="Advogado Criminalista e Socio administrativo "
+              phone="872311889"
+              email="harvey@gmail.com"
+              githubUrl="https://github.com"
+              linkedinUrl="https://linkedin.com"
+              twitterUrl="https://twitter.c0m"
+
+
+        />
+    </div>
+ )
+}
+
+export default App
